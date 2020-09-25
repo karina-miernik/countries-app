59d3ed2d5b9b6a760282412fc7040853f815ec6d
Karina Miernik <karina.miernik@gmail.com>
2020-09-25 13:13:09 +0200

    countries' item added

--- src/components/CountriesItem.js
+++ src/components/CountriesItem.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import styles from "./Styling.module.css";
-// import { Link } from "react-router";
+import { Link } from "react-router-dom";
+
 const CountriesItem = ({ title, capital, img, id }) => {
+
+  // const [countries, setCountries] = useState([])
+  // useEffect(() => {
+  //   fetch(`https://restcountries.eu/rest/v2/all`)
+  //     .then((r) => r.json())
+  //     .then((countries) => {
+  //       setCountries(countries)
+  //       console.log(countries);
+  //     });
+  // }, [])
+
   return (
+
+
+
     <Card
-      bg="dark"
-      variant="dark"
       style={{ width: "18rem" }}
       className={styles.countryItem}
     >
-      {/* <Link to={`countries/${id}`} className={styles.link}> */}{" "}
-      <Card.Img variant="top" src={img} />
-      {/* </Link> */}
+      <Link to={`/${id}`} className={styles.link}>
+
+        <Card.Img variant="top" src={img} />
+      </Link>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>Capital: {capital}</Card.Text>
-        <Button variant="primary">More</Button>
+        <div className={styles.countryButtonWrapper}>
+          <Link to={`/${id}`} className={styles.link}>
+            <button className={styles.countryButton}>More</button>
+          </Link>
+        </div>
       </Card.Body>
     </Card>
+
+
+
   );
 };
 
