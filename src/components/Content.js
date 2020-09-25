187df6745b43e090d7673490a9644aa72458045f
Karina Miernik <karina.miernik@gmail.com>
2020-09-25 15:19:45 +0200

    route name changed

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,16 @@
 import React from "react";
 
-import MainPage from './MainPage'
+import MainPage from "./MainPage";
 import CountriesDetails from "./CountriesDetails";
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
 const Content = () => {
   return (
     <>
-
       <Switch>
-        <Route exact path='/' component={MainPage} />
-        <Route path='/:id' component={CountriesDetails} />
+        <Route exact path="/countries-app" component={MainPage} />
+        <Route path="/:id" component={CountriesDetails} />
       </Switch>
-
-
     </>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import styles from './Styling.module.css'
+import styles from "./Styling.module.css";
 const Nav = () => {
   return (
-
     <nav className={styles.nav}>
-      <Link to={`/`} className={styles.link}>Find a country</Link>
-
+      <Link to={`/countries-app`} className={styles.link}>
+        Find a country
+      </Link>
     </nav>
   );
 };
