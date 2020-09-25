34cb38f0a4435170b428eaa25ae1bd68f7ab4398
Karina Miernik <karina.miernik@gmail.com>
2020-09-25 13:13:40 +0200

    navigation added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-
+import { Link } from "react-router-dom";
+import styles from './Styling.module.css'
 const Nav = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">Find a country</Navbar.Brand>
-    </Navbar>
+
+    <nav className={styles.nav}>
+      <Link to={`/`} className={styles.link}>Find a country</Link>
+
+    </nav>
   );
 };
 
