f061d089be43af9d7c712fa39d5d7a21fd423782
Karina Miernik <karina.miernik@gmail.com>
2020-09-25 15:15:05 +0200

    Updates

---
+++ precache-manifest.4ca7d6fc693fdcc3e18cc6a75d455d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aeb947931cf256bcb3c2c30ef6c2ee1",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "95cadf2be3d87cd68414",
+    "url": "/countries-app/static/css/main.20946b3a.chunk.css"
+  },
+  {
+    "revision": "062aeb8b5be2da443c3d",
+    "url": "/countries-app/static/js/2.c3803809.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/countries-app/static/js/2.c3803809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95cadf2be3d87cd68414",
+    "url": "/countries-app/static/js/main.6216a3a9.chunk.js"
+  },
+  {
+    "revision": "57004e892b69719c8568",
+    "url": "/countries-app/static/js/runtime-main.07c6d4b7.js"
+  }
+]);
