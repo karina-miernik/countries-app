c4acad78e5db2f1d79aa85d5c0c898862bb8fb5a
Karina Miernik <karina.miernik@gmail.com>
2020-09-25 13:13:29 +0200

    form added & functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-const Form = () => {
+import styles from "./Styling.module.css";
+
+const Form = ({ filter, onFormChange }) => {
+
+  const handleOnChange = (e) => {
+    onFormChange(e.target.value)
+  }
+
+
   return (
     <>
-      <div className="ml-3">
-        <InputGroup>
-          <FormControl
-            placeholder="Search"
-            aria-label="Search"
-            aria-describedby="basic-addon1"
-          />
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">
-              <svg
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                className="bi bi-search"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
-                />
-              </svg>
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-        </InputGroup>
+      <div className={styles.formWrapper}>
+        <FormControl className={styles.form}
+          placeholder="Search..."
+          aria-label="Search..."
+          onChange={handleOnChange}
+          value={filter}
+        />
       </div>
     </>
   );
